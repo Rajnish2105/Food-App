074d72a95793c1733c8b5c58ab8b4d7ffd8ca0e5
Rajnish2105 <166371359+Rajnish2105@users.noreply.github.com>
2024-12-01 16:46:20 +0530

    Responsive ui

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
@@ -6,16 +8,32 @@ import MainHeaderBackground from "./main-header-background";
 import Navlink from "./nav-link";
 
 export default function MainHeader(): JSX.Element {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNavHandler = () => {
+    setIsNavOpen((prevState) => !prevState);
+  };
+
   return (
     <>
       <MainHeaderBackground />
       <header className={classes.header}>
-        <Link className={classes.logo} href="/">
-          <Image src={logoImg} alt="Plate full of Food" priority />
-          NextLevel Food
-        </Link>
+        <div className={classes.logoContainer}>
+          <Link className={classes.logo} href="/">
+            <Image src={logoImg} alt="Plate full of Food" priority />
+            NextLevel Food
+          </Link>
+          <button
+            className={classes.menuToggle}
+            onClick={toggleNavHandler}
+            aria-label="Toggle Navigation"
+            aria-expanded={isNavOpen}
+          >
+            ☰
+          </button>
+        </div>
 
-        <nav className={classes.nav}>
+        <nav className={`${classes.nav} ${isNavOpen ? classes.navOpen : ""}`}>
           <ul>
             <li>
               <Navlink href="/meals">Browse Meals</Navlink>
